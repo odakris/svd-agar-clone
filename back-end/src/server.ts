5f3800cf149a073d3a364ec987dc9a7b8e652210
odakris <christopher_denis@live.fr>
2024-12-06 11:19:48 +0100

    initial commit

---
+++ back-end/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+const PORT = 3001;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="absolute top-10 right-10 z-10 text-2xl font-bold">AGAR.io Clone</h1>
+    </div>
+  );
+};
+
+export default App;
